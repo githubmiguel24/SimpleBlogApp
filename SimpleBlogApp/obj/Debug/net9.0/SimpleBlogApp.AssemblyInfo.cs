e73f1798e6fe66c2b4aaf2c1e05f74ffb85733c6
Charles Miguel Trinidad <trinidadcharlesmiguel04@gmail.com>
2025-11-10 20:52:33 +0800

    new

--- SimpleBlogApp/obj/Debug/net9.0/SimpleBlogApp.AssemblyInfo.cs
+++ SimpleBlogApp/obj/Debug/net9.0/SimpleBlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleBlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4682ab4b04d3db4343c1dbd4c7e38dd609c04b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee012a2cb454b9d8f84cd0fcafc102f4527811a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleBlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleBlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
